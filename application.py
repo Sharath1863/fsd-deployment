7054a79dd34b6b6356a22c53a682e21e19c77191
Sharath Chandra <sharathraghav2003@gmail.com>
2025-03-17 16:08:16 +0530

    test

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+
 application = Flask(__name__)  # Changed from app to application for Elastic Beanstalk compatibility
 CORS(application)
 
@@ -20,6 +21,7 @@ def completed():
 def test():
     return jsonify(status="Backend is test")
 
+
 @application.route('/status/<id>')
 def status_with_id(id):
     return jsonify(status=f"Backend is running for ID: {id}")
@@ -28,6 +30,10 @@ def status_with_id(id):
 def items():
     # Returning a list of items
     return jsonify(items=["item1", "item2", "item3"])
+
+@application.route('/test2/')
+def test2():
+    return jsonify(status="Backend is test2")
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=80)  # Ensure this runs on port 80 for Elastic Beanstalk
